058005103fe8cdc9014e6067cebe9996d919ada8
artjitt9611 <artjitt2512@gmail.com>
2023-01-10 11:16:46 +0700

    change faviccon adn Title

--- cm-area1/src/App.js
+++ cm-area1/src/App.js
@@ -1,10 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Footer } from "./Component/Footer";
 import { NavBar } from "./Component/Navbar";
 import { HashRouter as Router } from "react-router-dom";
 import AnimatedRoute from "./Component/AnimatedRoute";
 function App() {
+    const [title] = useState("กลุ่มส่งเสริมการศึกษาทางไกล เทคโนโลยีสารสนเทศและการสื่อสาร สพป.เชียงใหม่ เขต 1");
+
+    useEffect(() => {
+        document.title = title;
+    }, [title]);
+
     return (
         <Router>
             <NavBar />
