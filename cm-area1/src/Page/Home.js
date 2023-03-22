564e34c4c67366059f86010d1c12f5506d3fe165
artjitt9611 <artjitt2512@gmail.com>
2023-03-22 09:10:53 +0700

    add action

--- cm-area1/src/Page/Home.js
+++ cm-area1/src/Page/Home.js
@@ -1,19 +1,22 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { HomeBanner } from "../Component/HomeComponent";
 import { News } from "../Component/New";
 import { Skills } from "../Component/Skill";
 import { Contact } from "../Component/Contract";
 import { motion } from "framer-motion";
 import { VIDEO } from "../Component/Video";
-import ReactGA from 'react-ga';
-
+import ReactGA from "react-ga";
 
 const Home = () => {
     useEffect(() => {
-        ReactGA.pageview(window.location.pathname)
-    })
-
+        ReactGA.pageview(window.location.pathname);
+    });
     
+    ReactGA.event({
+        category: "User",
+        action: "Visit",
+    });
+
     return (
         <motion.div className="">
             <HomeBanner />
